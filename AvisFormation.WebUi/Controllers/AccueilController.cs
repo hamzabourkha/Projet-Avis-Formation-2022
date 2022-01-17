9c9528755883e2eae8ba09c42e67ed1e6782df21
hamzabourkha <hamza.bourkha@gmail.com>
2022-01-17 15:57:27 +0100

    Modification du 17/01/2022

--- AvisFormation.WebUi/Controllers/AccueilController.cs
+++ AvisFormation.WebUi/Controllers/AccueilController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AvisFormation.Data;
+using AvisFormation.WebUi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,7 +12,34 @@ namespace AvisFormation.WebUi.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var listDesFormations=new List<Formation>();
+            var listAVM = new List<AccueilViewModel>();
+            using (var context = new AvisFormationDbEntities())
+            {
+                listDesFormations = context.Formations.OrderBy(f=>Guid.NewGuid()).Take(4).ToList();
+                foreach (var f in listDesFormations)
+                {
+                    var avm = new AccueilViewModel();
+                    avm.Id = f.Id;
+                    avm.Nom = f.Nom;
+                    avm.Url = f.Url;
+                    avm.Description = f.Description;
+                    avm.NomSeo = f.NomSeo;
+                    if (f.Avis.Count > 0)
+                    {
+                        avm.Note = Math.Round(f.Avis.Average(a => a.Note),2);
+                        avm.NombreAvis = f.Avis.Count;
+                    }
+                    else
+                    {
+                        avm.Note = 0;
+                        avm.NombreAvis = 0;
+                    }
+                    listAVM.Add(avm);
+                }
+
+            }
+            return View(listAVM);
         }
 
         public ActionResult About()
